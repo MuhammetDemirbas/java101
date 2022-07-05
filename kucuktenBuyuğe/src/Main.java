b3e7e8e36281cd07f43b58d7a897d866478c20c7
MuhammetDemirbas <muhammetdemirbas383@gmail.com>
2022-07-05 21:23:31 +0300

    kucukten buyuğe dosyası eklendi

---
+++ kucuktenBuyuğe/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+	    int a,b,c;
+        System.out.println("1.sayıyı girin:");
+        a=inp.nextInt();
+        System.out.println("2.sayıyı girin:");
+        b=inp.nextInt();
+        System.out.println("3.Sayıyı girin:");
+        c= inp.nextInt();
+
+        if((a<b)&&(a<c)){
+            if(b<c){
+                System.out.println("a<b<c");
+            }else{
+                System.out.println("a<c<b");
+            }
+        }else if((b<a)&&(b<c)){
+            if(a<c){
+                System.out.println("b<a<c");
+            }else{
+                System.out.println("b<c<a");
+            }
+        }else if((c<a)&&(c<b)){
+            if(a<b){
+                System.out.println("c<a<b");
+            }else{
+                System.out.println("c<b<a");
+            }
+        }
+
+    }
+}
